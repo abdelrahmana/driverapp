065cb982addea474fe72653c1dc0eaf119e42ffc
abdelrahmana <truedream120@gmail.com>
2023-09-21 16:23:42 +0200

    saving home on map

--- app/src/main/java/com/tt/driver/ui/components/main/home/HomeFragment.kt
+++ app/src/main/java/com/tt/driver/ui/components/main/home/HomeFragment.kt
@@ -9,14 +9,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.common.api.Status
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.tt.driver.data.datastore.UserDataStore
+import com.tt.driver.data.models.entities.Order
+import com.tt.driver.data.models.http.OrdersResponse
 import com.tt.driver.ui.base.MapFragment
 import com.tt.driver.ui.components.main.MainActivity
+import com.tt.driver.ui.components.main.orders.OrdersFragmentDirections
+import com.tt.driver.ui.components.main.orders.OrdersViewModel
+import com.tt.driver.ui.components.main.orders.order_details.OrderViewModel
+import com.tt.driver.utils.show
 import com.tt.driver.utils.toLatLng
 import com.waysgroup.t7t_talbk_driver.R
 import com.waysgroup.t7t_talbk_driver.databinding.FragmentHomeBinding
@@ -30,6 +40,7 @@ import javax.inject.Inject
 class HomeFragment : MapFragment<FragmentHomeBinding>() {
 
     val viewModel by viewModels<HomeViewModel>()
+    val orderViewModel : OrdersViewModel by viewModels()
 
     @Inject
     lateinit var userDataStore: UserDataStore
@@ -53,6 +64,9 @@ class HomeFragment : MapFragment<FragmentHomeBinding>() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment?
         mapFragment?.getMapAsync {
             map = it
+            map?.clear()
+            orderViewModel.refreshOrders(1)
+
         }
 
         binding {
@@ -81,9 +95,60 @@ class HomeFragment : MapFragment<FragmentHomeBinding>() {
             }
 
         }
+        observeResult(orderViewModel.order) { // when getting order
+            if(it.data.meta.current_page == (it.data.meta.last_page ?: 1)) // no need to get more
+           else
+                orderViewModel.refreshOrders(it.data.meta.current_page+1)
+            setMarkersOnMapAndClickAction(it.data.data)
+
+        }
+
 
         initAutoCompleteSearch()
 
+    }
+    private fun setMarkersOnMapAndClickAction(result: List<Order>) { // set pins on map
+       // map?.clear() // remove
+        result.forEachIndexed { index, userProperties ->
+            val sydney = LatLng(userProperties.from_lat?.toDouble()?:0.0,
+                userProperties.from_long?.toDouble()?:0.0)
+            try {
+                val marker = map?.addMarker(
+                    MarkerOptions().position(sydney)/*.title("Marker in Sydney")*//*.icon(
+                        BitmapDescriptorFactory.fromBitmap(
+                            Bitmap.createScaledBitmap(
+                                bitMap!!,
+                                bitMap.width + 100,
+                                bitMap.height + 50,
+                                false
+                            )
+                        )
+                    )*/
+                )
+
+// Constrain the camera target to the Adelaide bounds.
+
+                //          googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 3f))
+                marker?.tag = index // array position
+                marker?.title = userProperties.from_name ?: ""
+            }catch (e : Exception){
+
+            }
+        }
+        map?.setOnMarkerClickListener { marker ->
+            marker.tag?.let{
+                val position:Int = marker.tag as Int
+                navigateTo(HomeFragmentDirections.actionHomeToOrderDetailsFragment(result.get(position).id ?: 0))
+
+                // val geocoder = Geocoder(requireContext(), Locale.getDefault())
+
+               // showDetailsProperties(result.get(position),geocoder)
+            }?:0
+
+            true
+            //Using position get Value from arraylist
+
+        }
     }
 
     private fun updateStatus(isOnline: Boolean) {
